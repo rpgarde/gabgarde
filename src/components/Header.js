65d6d1bbf4d032f7f3de7c9d76de05cfd06b7908
rpgarde <rpgarde@gmail.com>
2021-12-27 17:11:24 +1100

    removed conservation from navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#conservation">Conservation</Nav.Link>
+            {/* <Nav.Link href="#conservation">Conservation</Nav.Link> */}
             <Nav.Link href="#art">Art</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
