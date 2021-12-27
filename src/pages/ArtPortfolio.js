ff36b037d89eb0bb9697b07b911697a8480bf64c
rpgarde <rpgarde@gmail.com>
2021-12-27 18:26:49 +1100

    added image modal on click

--- src/pages/ArtPortfolio.js
+++ src/pages/ArtPortfolio.js
@@ -1,40 +1,63 @@
-import * as React from 'react';
+import React,{useState} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 import portfolioData from '../data/portfolioData'
+import ImageModal from '../components/ImageModal'
 
 const styles = {
     objectFit: 'cover',
     maxHeight: '10rem',
-    objectPosition: '50% 20%'
+    objectPosition: '50% 20%',
+    cursor:'pointer'
 }
 
 export default function ArtPortfolio() {
+    const [show, setShow] = useState(false);
+    const [modalData, setModalData] = useState({})
+    const handleClose = () => setShow(false);
+    const handleShow = (work) => {
+        setShow(true)
+        setModalData({
+            title:work.title,
+            descriptionLong:work.descriptionLong,
+            image:`/images/${work.image}`
+        })
+    };
 
     return (
         <div className="bg-light">
             <Container className="min-vh-100" id="art">
-                <h2 className="py-3 text-burgundy text-center">My personal art projects</h2>
+                <h2 className="pt-3 text-burgundy text-center">My personal art projects</h2>
+                <p className = "text-center">Click on any of the images for a more detailed write-up.</p>
                 <Row className="justify-content-evenly">
                     {portfolioData.map((work) => (
-                        <Col sm={12} md={6} lg={4}>
-                            <Card className="my-3 border-0 shadow-sm">
-                                <Card.Body>
-                                    <Card.Img variant="top" src={`/images/${work.image}`} style={styles} />
-                                    <Card.Title className="my-2 text-center">
-                                        <h5 className="text-burgundy">{work.title}</h5>
-                                    </Card.Title>
-                                    <Card.Text>{work.description}</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                        <>
+                            <Col sm={12} md={6} lg={4}>
+                                <Card className="mb-3 border-0 shadow-sm">
+                                    <Card.Body>
+                                        <Card.Img onClick = {()=>handleShow(work)} variant="top" src={`/images/${work.image}`} style={styles} />
+                                        <Card.Title className="my-2 text-center">
+                                            <h5 className="text-burgundy">{work.title}</h5>
+                                        </Card.Title>
+                                        <Card.Text>{work.description}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </>
                     ))}
-                </Row>
-            </Container>
-        </div>
+                    <ImageModal
+                                modalData = {modalData}
+                                show = {show}
+                                handleClose = {handleClose}
+                    />
+            </Row>
+        </Container>
+        </div >
     )
 }
