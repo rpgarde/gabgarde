cda16633c29e4b74e9e9a11cea77dc3eee5bd5e6
rpgarde <rpgarde@gmail.com>
2021-12-27 17:25:16 +1100

    deworded captions

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -7,17 +7,17 @@ const portfolioData = [
     {
         title:"Mama Coco",
         image:"coco.jpg",
-        description:`This doll was sculpted in polymer clay (Sculpey Living Doll) on a wire armature. A "bodysuit" was then sewn and stuffed with polyester wadding. The dress was hand-sewn cheesecloth, with a hand-painted design.\nThe wheelchair's base is cardboard and wire. Raffia paper was woven and coated in wood glue for the backrest, the sides of the armrests, and the footrest. Twine was wrapped around the frame. The wheels are polymer clay, wire, and toothpicks.`
+        description:`This doll was sculpted in polymer clay (Sculpey Living Doll) on a wire armature. A "bodysuit" was then sewn and stuffed with polyester wadding. The dress was hand-sewn cheesecloth, with a hand-painted design. The wheelchair's base is cardboard and wire. Raffia paper was woven and coated in wood glue for the backrest, the sides of the armrests, and the footrest. Twine was wrapped around the frame. The wheels are polymer clay, wire, and toothpicks.`
     },
     {
         title:"Fletcher St. Pierre, The Cane Toad",
         image:"fletcher.jpg",
-        description:`Inspired by the Toad in Rango, and Mr. Toad from The Wind in the Willows. His head, hands, and feet were sculpted in "cold porcelain", which is actually just a microwaved mixture of cornstarch and glue. The clothes were handsewn. The hat was made by pressing frayed acrylic yarn, soaked in glue, onto a cardboard base. In effect, this was like making felt. I put a little feather in the hatband. He also has a gold-capped walking stick, made with an old paintbrush and epoxy putty.`
+        description:`Inspired by the Toad in Rango, and Mr. Toad from The Wind in the Willows. His head, hands, and feet were sculpted in "cold porcelain", a microwaved mixture of cornstarch and glue. The clothes were handsewn. The hat was made by pressing frayed acrylic yarn, soaked in glue, onto a cardboard base. I put a little feather in the hatband. He also has a gold-capped walking stick, made with an old paintbrush and epoxy putty.`
     },
     {
         title:"Lola Bas",
         image:"lolabas.jpg",
-        description:`I started making this on the first anniversary of the passing of Salvacion “Lola Bas” Calderon, whom the bust is based on. Lola Bas signed on to be our family nanny and cook when she was in her early 20s, and passed away in our home at the age of 86. She is made from polymer clay, acrylic eyes, scrap fabric, and local alpaca hair.`
+        description:`I started making this on the 1st anniversary of the passing of Salvacion “Lola Bas” Calderon, whom the bust is based on. She signed on to be our family nanny and cook in her early 20s, and passed away in our home at the age of 86. She is made from polymer clay, acrylic eyes, scrap fabric, and local alpaca hair.`
     },
     {
         title:"Uncle Iroh",
