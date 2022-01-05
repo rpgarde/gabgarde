6abbd78bbd011250eb9d7e4660dc0af205de3299
rpgarde <rpgarde@gmail.com>
2022-01-05 13:34:30 +1100

    added skills data

---
+++ src/data/skillsData.js
@@ -0,0 +1,61 @@
+const skills = [
+    {
+        category: "Examination and Testing",
+        content: [
+            'Paper examination',
+            'Wood examination', 
+            'Ceramics examination',
+            'Reading XRF, FTIR, and XRD data to identify pigments, adhesives, and substrates',
+            'Inorganic and organic materials chemistry and microscopy',
+            'Solubility testing',
+        ]
+
+    },
+    {
+        category: "Interventive",
+        content: [
+            'Wooden object repairs (Paraloid B-72, fish glue, Plextol B500, Flugger acrylic filler)',
+            'Basket repair with wheat starch paste', 
+            'Metals cleaning (acetone, ethanol, sodium hydroxide, commercial cleaners)',
+            'Colour matching',
+            'Infilling (Flugger acrylic filler, methyl cellulose and calcium carbonate fill, plaster of paris, epoxy putty with Paraloid B-72 barrier)',
+            'Inpainting (gouache, acrylic, watercolour)',
+            'Use of heated spatula to remove creases',
+            'Textile stabilisation',
+            'Textile repair',
+            'Humidification',
+        ]
+
+    },
+    {
+        category: "Cleaning",
+        content: [
+            'Dry cleaning (brush and vacuum, plastic eraser, Groomstick)',
+            'Wet cleaning (sodium lauryl sulphate, Orvus paste, ethanol, citric acid)', 
+        ]
+
+    },
+    {
+        category: "Mounting",
+        content: [
+            'Manufacturing mounting systems (machine-sewn velcro mounts, paper-lined magnetic board mounts)',
+            'Textile mounting (hand-stitched)',
+        ]
+
+    },
+    {
+        category: "Administrative",
+        content: [
+            'Condition reporting with mylar overlays on photographs to indicate damage',
+            'KE EMu and CM9 Training', 
+            'Treatment reports',
+            'Photography assistance',
+            'Photograph renaming and organisation',
+            'Detailed photographic record sheets',
+            'Preservation Needs Assessment'
+        ]
+
+    }
+]
+
+export default skills
