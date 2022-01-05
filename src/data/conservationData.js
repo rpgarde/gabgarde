cfdb3dc59a478ba04356813e0d964911dc3c739a
rpgarde <rpgarde@gmail.com>
2022-01-05 13:52:28 +1100

    added conservation work

--- src/data/conservationData.js
+++ src/data/conservationData.js
@@ -9,20 +9,6 @@ const portfolioData = [
         ]
     },
     {
-        title:"Repair of Ernabella batik silk for private client",
-        dates:"August 2021 - September 2021",
-        description:[
-            "Repaired tears in a silk textile and removed creases in preparation for display."
-        ]
-    },
-    {
-        title:"Pattern and Print: Easton Pearson Archive",
-        dates:"October 2020 - February 2021",
-        description:[
-            "Stabilised, repaired, and condition-reported garments in preparation for a national touring exhibition.",
-        ]
-    },
-    {
         title:"Repair of Provençal Santon Habillé (French folk doll) for conservation treatment course",
         dates:"November 2021 - December 2021",
         description:[
@@ -35,6 +21,20 @@ const portfolioData = [
         description:[
             "Conducted a preservation needs assessment for the SPOSA collection, and made several recommendations for the future management of the collection store.", 
             "Authored a detailed report in accordance with AICCM guidelines."
+        ]
+    },
+    {
+        title:"Repair of Ernabella batik silk for private client",
+        dates:"August 2021 - September 2021",
+        description:[
+            "Repaired tears in a silk textile and removed creases in preparation for display."
+        ]
+    },
+    {
+        title:"Pattern and Print: Easton Pearson Archive",
+        dates:"October 2020 - February 2021",
+        description:[
+            "Stabilised, repaired, and condition-reported garments in preparation for a national touring exhibition.",
         ]
     },
 ]
--- src/pages/Conservation.js
+++ src/pages/Conservation.js
@@ -6,24 +6,35 @@ import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import conservationData from '../data/conservationData'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
 
 export default function Conservation() {
 
     return (
-        <div className = "bg-light">
+        <div className="bg-light">
             <Container className="min-vh-100" id="conservation">
-                <h2 className = "py-3 text-burgundy text-center">My conservation work</h2>
-                <Row xs={1} md={2} className="g-4 justify-content-evenly">
-                {conservationData.map((work)=>(
-                    <Card style={{ width: '18rem' }} className = "m-3 border-0 shadow-sm">
-                        <Card.Body>
-                        <Card.Img variant="top" src={`/images/${work.image}`} />
-                            <Card.Title>{work.title}</Card.Title>
-                            <Card.Text>{work.description}</Card.Text>
-                        </Card.Body>
-                    </Card>
-
-                ))}
+                <h2 className="py-3 text-burgundy text-center">My conservation work</h2>
+                <Row className="justify-content-start">
+                    {conservationData.map((work) => (
+                        <Col sm={12} md={6} lg={4} className="d-flex align-self-stretch">
+                            <Card className="mb-3 border-0 shadow-sm">
+                                <Card.Body>
+                                    <Card.Title className = "my-2">
+                                    <h5 className="text-burgundy">{work.title}</h5>
+                                        </Card.Title>
+                                    <Card.Subtitle className = "my-2 text-muted">{work.dates}</Card.Subtitle>
+                                <ul>
+                                    {work.description.map(item => (
+                                        <li>
+                                            {item}
+                                        </li>
+                                    ))}
+                                </ul>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </div>
