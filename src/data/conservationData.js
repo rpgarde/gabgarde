b5a65142e2bda79755c0e413500f783bdb9a0234
rpgarde <rpgarde@gmail.com>
2022-01-05 13:29:13 +1100

    added conservation projects

--- src/data/conservationData.js
+++ src/data/conservationData.js
@@ -1,33 +1,41 @@
 const portfolioData = [
     {
-        title:"Brutus The Chimpanzee",
-        image:"brutus1.png",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem suscipit expedita voluptatum id animi reprehenderit? Quibusdam, laudantium amet! Iste quasi minima non eius consectetur maiores labore, facilis delectus dolor!"
+        title:"10th Asia-Pacific Triennial (APT10) at the Queensland Art Gallery/Gallery of Modern Art (QAGOMA)",
+        dates:"August 2021 - December 2021",
+        description:[
+            "Conducted object treatments under supervision of the QAGOMA conservators, and assisted in mounting, condition reporting, and photography for APT10.",
+            "Treatments included infilling, adhesion, inpainting, humidification, and wet and dry cleaning.",
+            "Objects included textile artworks, wooden objects, and woven artworks."
+        ]
     },
     {
-        title:"Brutus The Chimpanzee",
-        image:"brutus1.png",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem suscipit expedita voluptatum id animi reprehenderit? Quibusdam, laudantium amet! Iste quasi minima non eius consectetur maiores labore, facilis delectus dolor!"
+        title:"Repair of Ernabella batik silk for private client",
+        dates:"August 2021 - September 2021",
+        description:[
+            "Repaired tears in a silk textile and removed creases in preparation for display."
+        ]
     },
     {
-        title:"Brutus The Chimpanzee",
-        image:"brutus1.png",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem suscipit expedita voluptatum id animi reprehenderit? Quibusdam, laudantium amet! Iste quasi minima non eius consectetur maiores labore, facilis delectus dolor!"
+        title:"Pattern and Print: Easton Pearson Archive",
+        dates:"October 2020 - February 2021",
+        description:[
+            "Stabilised, repaired, and condition-reported garments in preparation for a national touring exhibition.",
+        ]
     },
     {
-        title:"Brutus The Chimpanzee",
-        image:"brutus1.png",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem suscipit expedita voluptatum id animi reprehenderit? Quibusdam, laudantium amet! Iste quasi minima non eius consectetur maiores labore, facilis delectus dolor!"
+        title:"Repair of Provençal Santon Habillé (French folk doll) for conservation treatment course",
+        dates:"November 2021 - December 2021",
+        description:[
+            "Re-constructed lost elements (figure’s feet and base) with two-part epoxy putty, over a barrier layer of 20% Paraloid B-72 in acetone, and in-painted to match the original material."
+        ]
     },
     {
-        title:"Brutus The Chimpanzee",
-        image:"brutus1.png",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem suscipit expedita voluptatum id animi reprehenderit? Quibusdam, laudantium amet! Iste quasi minima non eius consectetur maiores labore, facilis delectus dolor!"
-    },
-    {
-        title:"Brutus The Chimpanzee",
-        image:"brutus1.png",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolorem suscipit expedita voluptatum id animi reprehenderit? Quibusdam, laudantium amet! Iste quasi minima non eius consectetur maiores labore, facilis delectus dolor!"
+        title:"Preservation Needs Assessment at St. Peters Lutheran College’s Old Scholars Archive (SPOSA)",
+        dates:"May 2021 - July 2021",
+        description:[
+            "Conducted a preservation needs assessment for the SPOSA collection, and made several recommendations for the future management of the collection store.", 
+            "Authored a detailed report in accordance with AICCM guidelines."
+        ]
     },
 ]
 
